e98d482b2e78932c5de4e70a1743d0db4cd440b4
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 02:08:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(foods)
-  if foods.include?("cheddar", "gouda", "camembert")
+  if foods.detect{|i| i.is_a?(String)}
     return food
   else 
     return nil
