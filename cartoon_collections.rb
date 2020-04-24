4388f4b68ac932e0aaef97a00eac5fbe44397fc4
Christine Zosche <christine.zosche@gmail.com>
2020-04-24 02:03:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,14 +20,11 @@ def find_the_cheese(foods)
   if foods.include? do |food|
     food == "cheddar"
     return "cheddar"
-    end
   elsif foods.include? do |food|
     food == "gouda"
     return "gouda"
-    end
   elsif foods.include? do |food|
     food == "camembert"
     return = "camembert"
-    end
   end
 end
